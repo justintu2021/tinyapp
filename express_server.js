f2487027c5ee40f6c02370732e26d4b92d1f7ab5
Justin Tu <coppolavina@gmail.com>
2021-11-11 04:30:31 +0000

    creating login form and displaying username

--- express_server.js
+++ express_server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 const bodyParser = require("body-parser");
 const { urlencoded } = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+////////////////////////////////////////////////////////////
 
 function generateRandomString() {
     let result           = '';
@@ -42,15 +45,18 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
+  templateVars["username"]= req.cookies["username"];
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username : req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  templateVars["username"]= req.cookies["username"];
   res.render("urls_show", templateVars);
 });
 
@@ -60,6 +66,7 @@ app.post("/urls", (req, res) => {
   urlDatabase[randomURL] = req.body["longURL"] /// add new shortURL & long URL to database
   const longURL = urlDatabase[randomURL]
   const templateVars = { shortURL: randomURL, longURL: longURL};
+  templateVars["username"]= req.cookies["username"];
   res.render("urls_show", templateVars);
 });
 
@@ -83,5 +90,12 @@ app.post("/urls/:shortURL", (req,res) => {
   urlDatabase[shortURL] = longURL;
   console.log(urlDatabase[shortURL])
   console.log(urlDatabase)
+  res.redirect("/urls")
+})
+
+app.post('/login', (req,res) =>{
+  const { username } = req.body
+  res.cookie("username", username)
+  console.log(username)
   res.redirect("/urls")
 })
